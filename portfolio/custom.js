15d0c61402cb16d5f9dbba46a05b53d93fdd16ad
unknown <yongshisun3@gmail.com>
2020-10-25 10:44:26 -0700

    2/3 pages done

--- portfolio/custom.js
+++ portfolio/custom.js
@@ -1,4 +1,33 @@
-$(document).ready(function(){
-    var posX = window.innerWidth;
-    var posY = window.innerHeight;
+
+
+// linking html custom classes with a variable in javascript
+const cards = document.querySelector(".cards");
+const popup = document.querySelector(".popup-box");
+const popupClose = popup.querySelector(".popup-close")
+
+
+// if a button is clicked from the card, open pop up
+cards.addEventListener("click", function(event){
+
+    if(event.target.tagName.toLowerCase() == "button"){
+
+        //linking custom classes to variables
+        const frame = event.target.parentElement;
+        const h3 = frame.querySelector("h3").innerHTML;
+        const more = frame.querySelector(".more").innerHTML;
+
+        //linking custom classes to template classes
+        popup.querySelector("h3").innerHTML= h3;
+        popup.querySelector(".popup-body").innerHTML= more;
+        popupBox();
+    }
 })
+
+//if a button is click from the popup window, close pop up
+popupClose.addEventListener("click",popupBox);
+
+
+//popup function
+function popupBox(){
+    popup.classList.toggle("open");
+}
